c031842a38cccb611473fd07dc149c68730c1561
chadmx <chad.mav@outlook.com>
2015-09-29 06:58:15 +0000

    Used a hash represent paths for easier look up

--- Travelling Salesman Problem/tsp_1.rb
+++ Travelling Salesman Problem/tsp_1.rb
@@ -1,12 +1,17 @@
-# Greedy algorithm(Nearest Neighbour Solution)
+# Nearest Neighbour Solution
 
 v = ['b','c','d','e'] #Vertices
 a = [['b','c'],['b','d'],['b','e'],['c','d'],['c','e'],['e','d']] #Edges
 c = [5,20,6,7,3,8] #Costs
 g = [a,v] #Graph
-h = [['b',50],['c', 30],['d',0],['e',41]] #Heuristics
+h = {
+   'b'=>50,
+   'c'=>30,
+   'd'=>0,
+   'e'=>41
+} #Heuristics
 
-paths = [] #Path Costs
+paths = {} #Path Costs
 visited = [] #Visited vertices
 deadend = [] #Vertices that are dead ends(Loop avoidance).
 
@@ -21,5 +26,8 @@ finish = v[2] #Finish at vertex, D
 a.each{ |e|
    i = a.index(e)
    ci = c[i]
-   paths << e + [ci]
+   paths[e] = ci
 }
+
+# p paths or puts paths.inspect
+# {["b", "c"]=>5, ["b", "d"]=>20, ["b", "e"]=>6, ["c", "d"]=>7, ["c", "e"]=>3, ["e", "d"]=>8} Paths hash when inspected.
